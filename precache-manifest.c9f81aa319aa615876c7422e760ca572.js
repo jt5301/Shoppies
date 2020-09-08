6a7f5d569de94606f9e5b97d0950b284952c754d
John Tieu <johnson.tieu@gmail.com>
2020-09-08 02:57:12 -0400

    Updates

---
+++ precache-manifest.c9f81aa319aa615876c7422e760ca572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b57b9236dd9491cdb764bf55986bab",
+    "url": "/Shoppies/index.html"
+  },
+  {
+    "revision": "ded639bc040e6e66d7dc",
+    "url": "/Shoppies/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "930166c24c96f40995cf",
+    "url": "/Shoppies/static/js/2.c3923f60.chunk.js"
+  },
+  {
+    "revision": "ded639bc040e6e66d7dc",
+    "url": "/Shoppies/static/js/main.0cd840e0.chunk.js"
+  },
+  {
+    "revision": "8849c6b79a0f5013baf0",
+    "url": "/Shoppies/static/js/runtime~main.0d2f99f4.js"
+  }
+]);
